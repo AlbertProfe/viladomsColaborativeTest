3edf4e45cd4a6c4d3f23d76913343a97b58bb748
derryx <derryx@DESKTOP-9I86LMF>
2021-05-07 20:44:43 +0200

    Commit starting Class Person Only atributes With many failures

---
+++ exampleColaborativeTest1/src/exampleColaborativeTest1/Person.java
@@ -0,0 +1,14 @@
+package exampleColaborativeTest1;
+
+public class Person {
+	
+	private String name;
+	private int years;
+	private MyDate BirthDate;
+	private double height;
+	private int weight;
+	
+	
+	
+	
+}
