c368aaa634d29dcb363e9632a4b0573902579bed
derryx <derryx@DESKTOP-9I86LMF>
2021-05-07 20:58:30 +0200

    commit Class Person from 0

--- exampleColaborativeTest1/src/exampleColaborativeTest1/Person.java
+++ exampleColaborativeTest1/src/exampleColaborativeTest1/Person.java
@@ -1,14 +1,5 @@
 package exampleColaborativeTest1;
 
 public class Person {
-	
-	private String name;
-	private int years;
-	private MyDate BirthDate;
-	private double height;
-	private int weight;
-	
-	
-	
-	
+
 }
