632348a6b4f3fd56e66799854aad1df4a013396c
User <User@DESKTOP-5D1LJ47>
2021-05-07 19:40:28 +0200

    master commint

--- exampleColaborativeTest1/src/exampleColaborativeTest1/Init.java
+++ exampleColaborativeTest1/src/exampleColaborativeTest1/Init.java
@@ -3,8 +3,14 @@ package exampleColaborativeTest1;
 public class Init {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		
+		
+		System.out.println("Welcome to failure and coplexity .... yeahhh");
+		System.out.println("this is master ...");
+		
+		System.out.println("We are working at branch 1 ....");
 	}
 
 }
